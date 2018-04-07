10ef9fbd7234eb733a1d82a2c42463a9776d4d45
Gustavo <gcdpinho@gmail.com>
2018-04-07 01:57:30 -0300

    desc

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
 jQuery(function ($) {
     'use strict';
+
     showTables();
+
     $('.tabela').change(function () {
+        $('.page-loader-wrapper').fadeIn();
+        var is = $(this);
         var tabela = $(this).find(':selected').val();
-        if (tabela != "doenca") {
-            $(this).parents('.bloco').find('.demais').css('display', 'block');
-            $(this).parents('.bloco').find('.doenca').css('display', 'none');
-        } else {
-            $(this).parents('.bloco').find('.demais').css('display', 'none');
-            $(this).parents('.bloco').find('.doenca').css('display', 'block');
-        }
+        $.ajax({
+            type: "POST",
+            url: backend.url + "utility/descTable",
+            data: {
+                table: tabela
+            },
+            success: function (response) {
+                console.log(response);
+                console.log($(this))
+                $(is).parents('.bloco').append('<div class="row v-center">'+
+                        '<div class="col-md-5">'+
+                            '<div class="form-group">'+
+                                '<label>Selecione um atributo:</label>'+
+                                '<select class="form-control attr">'+
+                                '</select>'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="col-md-5">'+
+                            '<div class="form-group">'+
+                                '<label>Valor do atributo:</label>'+
+                                '<input type="text" class="form-control valueAttr">'+
+                            '</div>'+
+                        '</div>'+
+                        '<div class="col-md-2 buttons">'+
+                            '<button type="button" class="btn btn-success" onclick="add(this)">+</button>'+
+                            '<button type="button" class="btn btn-danger" onclick="remove(this)">-</button>'+
+                        '</div>'+
+                    '</div>');
+                $('.page-loader-wrapper').fadeOut();
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
     });
 
 });
@@ -37,7 +68,7 @@ var showTables = function () {
             options.push('<option value="" disabled selected>Selecione uma opção</option>');
             for (var element of response)
                 if (!(element.Tables_in_u709009684_wsaud.indexOf('doenca') >= 0 && element.Tables_in_u709009684_wsaud != "doenca"))
-                    options.push("<option>" + element.Tables_in_u709009684_wsaud + "</option>");
+                    options.push("<option value='" + element.Tables_in_u709009684_wsaud + "'>" + acentuacaoTable(element.Tables_in_u709009684_wsaud) + "</option>");
             $('.tabela').html(options);
             $('.page-loader-wrapper').fadeOut();
         },
@@ -45,4 +76,27 @@ var showTables = function () {
             console.log(error);
         }
     });
+}
+
+var acentuacaoTable = function (table) {
+    switch (table) {
+        case "doenca":
+            return "Doença";
+        case "evolucao":
+            return "Evolução";
+        case "localizacao":
+            return "Localização";
+        case "cor":
+            return "Cor";
+        case "lesaoFundamental":
+            return "Lesão Fundamental";
+        case "palpacao":
+            return "Palpação";
+        case "crescimento":
+            return "Crescimento";
+        case "superficie":
+            return "Superfície";
+        case "trauma":
+            return "Trauma";
+    }
 }
