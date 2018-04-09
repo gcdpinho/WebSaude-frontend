38382c02fb84e908f1a9e757950b50baf3d2114d
gcdpinho <gcdpinho@gmail.com>
2018-04-09 00:34:06 -0300

    layout done

--- js/main.js
+++ js/main.js
@@ -4,10 +4,81 @@ jQuery(function ($) {
     showTables();
 
     $('.tabela').change(changeTabela);
+
+    $('#formCadastro').submit(function (e) {
+        if (validation()) {
+            var data = generateQueries();
+            $('.page-loader-wrapper').fadeIn();
+            $.ajax({
+                type: "POST",
+                url: backend.url + "utility/insertAll",
+                data: {
+                    data: data
+                },
+                success: function (response) {
+                    console.log(response);
+                    $('.page-loader-wrapper').fadeOut();
+                },
+                error: function (error) {
+                    console.log(error.message);
+                    $('.page-loader-wrapper').fadeOut();
+                }
+            });
+            e.preventDefault();
+        }
+    });
 });
 
+var validation = function () {
+    var valid = true;
+
+    /* Selects de tabelas que não possuem uma opção selecionada*/
+    $('select.tabela').each(function (e, element) {
+        if ($(element).find(':selected').val() == "") {
+            valid = false;
+            $(element).addClass('error');
+        }
+    });
+
+    /* Selects de atributos que não possuem uma opção selecionada*/
+    $('select.attr').each(function (e, element) {
+        if ($(element).find(':selected').val() == "") {
+            valid = false;
+            $(element).addClass('error');
+        }
+    });
+
+    /* Inputs vazios que não permitem valores nulos*/
+    $('input.valueAttr').each(function (e, element) {
+        if ($(element).val() == "" && $(element).parents('.rowAttr').find('select.attr').find(':selected').attr('name') == "NO") {
+            valid = false;
+            $(element).addClass('error');
+        }
+    });
+
+    return valid;
+}
+
+var generateQueries = function () {
+    var data = [];
+    var row;
+    var atributos;
+    var tabelas = $('select.tabela').find(':selected');
+    for (var i = 0; i < tabelas.length; i++) {
+        row = {};
+        row['tabela'] = $(tabelas[i]).val();
+        atributos = $(tabelas[i]).parents('.bloco').find('.rowAttr');
+        row['atributos'] = {};
+        for (var j = 0; j < atributos.length; j++)
+            row['atributos'][$(atributos[j]).find('select.attr option:selected').val()] = $(atributos[j]).find('input.valueAttr').val();
+        data.push(row);
+    }
+    console.log(data);
+    return data;
+}
+
 var add = function (e) {
-    $('form#formCadastro').append('<div class="bloco">' +
+    $('div.formContent').append('<div class="bloco">' +
         '<div class="row v-center">' +
         '<div class="col-md-10">' +
         '<div class="form-group">' +
@@ -36,7 +107,7 @@ var remove = function (e) {
         $(e).parents('.bloco').remove();
 }
 
-var removeAttr = function (e){
+var removeAttr = function (e) {
     if ($(e).parents('.bloco').find('.attr').length > 1)
         $(e).parents('.row.v-center').remove();
 }
@@ -96,7 +167,7 @@ var acentuacao = function (table) {
             return "Fumo";
         case "flgAlcool":
             return "Àlcool";
-        
+
     }
 }
 
@@ -112,7 +183,11 @@ var appendAttr = function (bloco) {
         '<div class="col-md-5">' +
         '<div class="form-group">' +
         '<label>Valor do atributo:</label>' +
-        '<input type="text" class="form-control valueAttr" disabled>' +
+        '<select class="form-control aux" disabled>' +
+        '<option value="" disabled selected>Selecione uma opção</option>' +
+        '</select>' +
+        '<select class="form-control valueAttr selectpicker" title="Selecione uma opção" disabled multiple>' +
+        '</select>' +
         '</div>' +
         '</div>' +
         '<div class="col-md-2 buttonsAttr">' +
@@ -121,11 +196,119 @@ var appendAttr = function (bloco) {
         '</div>' +
         '</div>');
     var aux = $(bloco).find('.attr');
-    $(aux[aux.length-1]).append(optionsAttr);
-    $('.attr').change(function () {
-        $(this).parents('.row').find('input').attr('disabled', false);
-        if ($(this).find(':selected').attr('name') != "NO")
-            $(this).parents('.row').find('.buttonsAttr').css('display', 'block');
+    $(aux[aux.length - 1]).append(optionsAttr);
+    $('select.attr').change(function () {
+        $('.page-loader-wrapper').fadeIn();
+        var is = $(this);
+        $(this).removeClass('error');
+        $.ajax({
+            type: "POST",
+            url: backend.url + "utility/selectByAtributoByTable",
+            data: {
+                atributo: $(this).find(':selected').val(),
+                table: $(this).parents('.bloco').find('.tabela option:selected').val()
+            },
+            success: function (response) {
+                console.log(response);
+                $(is).parents('.row').find('select.valueAttr').append(response.map((e) => {
+                    return "<option val='" + e.id + "'>" + e.nome + "</option>"
+                }));
+                $(is).parents('.row').find('select.aux').remove();
+                $(is).parents('.row').find('.valueAttr').attr('disabled', false);
+                $(is).parents('.row').find('.selectpicker').selectpicker();
+                $('.page-loader-wrapper').fadeOut();
+            },
+            error: function (error) {
+                console.log(error);
+                $('.page-loader-wrapper').fadeOut();
+            }
+        });
+    });
+    $('input.valueAttr').on('input', function (e) {
+        $(this).removeClass('error');
+    });
+    $('.page-loader-wrapper').fadeOut();
+}
+
+var appendDoenca = function (bloco) {
+    $(bloco).append('<div class="row v-center rowAttr">' +
+        '<div class="col-md-5">' +
+        '<div class="form-group">' +
+        '<label>Selecione um atributo:</label>' +
+        '<select class="form-control attrDoenca">' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-md-5">' +
+        '<div class="form-group value">' +
+        '<label>Valor do atributo:</label>' +
+        '<input type="text" class="form-control valueAttr" disabled>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-md-2 buttonsAttr">' +
+        '<button type="button" class="btn btn-success" onclick="addAttr(this)">+</button>' +
+        '<button type="button" class="btn btn-danger" onclick="removeAttr(this)">-</button>' +
+        '</div>' +
+        '</div>');
+    var aux = $(bloco).find('.attrDoenca');
+    $(aux[aux.length - 1]).append(optionsAttr);
+    $('select.attrDoenca').change(function () {
+        var value = $(this).find(":selected").val();
+        if (value != "nome" && value != "tipo") {
+            $(this).parents('.row').find('.valueAttr').remove();
+            if (value == "insercao") {
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção" multiple>' +
+                    '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="0">Sésil</option>' +
+                    '<option value="1">Pediculada</option>'
+                );
+            } else if (value == "dor") {
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção" multiple>' +
+                    '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="0">Sim</option>' +
+                    '<option value="1">Não</option>'
+                );
+            } else if (value == "comeco") {
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção" multiple>' +
+                    '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="0">Súbito</option>' +
+                    '<option value="1">Insidioso</option>'
+                );
+            } else if (value == "trauma"){
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção" multiple>' +
+                '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="0">Sim</option>' +
+                    '<option value="1">Não</option>'
+                );
+            }
+            else if (value == "flgRemovida"){
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção">' +
+                '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="true">Sim</option>' +
+                    '<option value="false">Não</option>'
+                );
+            }
+            else if (value == "flgFumo"){
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção">' +
+                '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="true">Sim</option>' +
+                    '<option value="false">Não</option>'
+                );
+            }
+            else if (value == "flgAlcool"){
+                $(this).parents('.row').find('div.value').append('<select class="form-control valueAttr selectpicker" title="Selecione uma opção">' +
+                '</select>');
+                $(this).parents('.row').find('select.valueAttr').append('<option value="true">Sim</option>' +
+                    '<option value="false">Não</option>'
+                );
+            }
+
+            $(this).parents('.row').find('select.valueAttr').selectpicker();
+        } else{
+            $(this).parents('.row').find('.valueAttr').remove();
+            $(this).parents('.row').find('div.value').append('<input type="text" class="form-control valueAttr" disabled>');
+            $(this).parents('.row').find('input').attr('disabled', false);
+        }
     });
     $('.page-loader-wrapper').fadeOut();
 }
@@ -134,6 +317,7 @@ var optionsAttr = [];
 var changeTabela = function () {
     $('.page-loader-wrapper').fadeIn();
     var is = $(this);
+    $(is).removeClass('error');
     var tabela = $(this).find(':selected').val();
     $.ajax({
         type: "POST",
@@ -148,7 +332,10 @@ var changeTabela = function () {
             for (var i = 1; i < response.length; i++)
                 optionsAttr.push("<option value='" + response[i].Field + "' name='" + response[i].Null + "'>" + acentuacao(response[i].Field) + "</option>");
             $(is).parents('.bloco').find('.rowAttr').remove();
-            appendAttr($(is).parents('.bloco'));
+            if (tabela != "doenca")
+                appendAttr($(is).parents('.bloco'));
+            else
+                appendDoenca($(is).parents('.bloco'));
         },
         error: function (error) {
             console.log(error);
